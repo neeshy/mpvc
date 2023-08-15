4f08432f37dfc6fe98713a53f9668adf5f7cef95
neeshy <neeshy@tfwno.gf>
2023-08-14 23:39:49 -0400

    Take IntoIterator in command_arg and Iterator in _command

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,11 @@ extern crate serde_json;
 
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 use std::io::{BufRead, BufReader, Error as IoError, ErrorKind as IoErrorKind, Write};
+use std::iter::once;
 use std::os::unix::net::UnixStream;
 
 use log::debug;
-use serde_json::{json, Error as JsonError, Map, Number, Value};
+use serde_json::{Error as JsonError, Map, Number, Value};
 
 pub struct Mpv {
     path: String,
@@ -92,9 +93,14 @@ impl Mpv {
         self._disconnect();
     }
 
-    fn _command(&mut self, command: &[Value]) -> Result<Value, Error> {
+    fn _command<I: Iterator<Item = Value>>(&mut self, command: I) -> Result<Value, Error> {
         self.counter += 1;
-        let c = json!({"command": command, "request_id": self.counter}).to_string();
+        let c = Value::Object({
+            let mut m = Map::with_capacity(2);
+            m.insert("command".to_string(), command.collect());
+            m.insert("request_id".to_string(), self.counter.into());
+            m
+        }).to_string();
         debug!("Command: {}", c);
         self.reader.get_ref().write_all((c + "\n").as_bytes()).map_err(Error::WriteError)?;
         loop {
@@ -143,31 +149,16 @@ impl Mpv {
         }
     }
 
-    /// Run an mpv command. The arguments are passed as a JSON array.
+    /// Run an mpv command. The arguments are passed as a collection whose elements can be
+    /// converted to JSON values.
     ///
     /// ```
     /// let mut mpv = Mpv::connect("/tmp/mpvsocket")?;
-    /// mpv.command_value("quit", &[4.into()])?;
+    /// mpv.command_arg("seek", ["0", "absolute"])?;
     /// ```
-    pub fn command_value(&mut self, command: &str, args: &[Value]) -> Result<(), Error> {
-        let mut a = Vec::with_capacity(args.len() + 1);
-        a.push(command.into());
-        a.extend(args.iter().cloned());
+    pub fn command_arg<I: IntoIterator<Item = V>, V: Into<Value>>(&mut self, command: &str, args: I) -> Result<(), Error> {
         // XXX: Drop return value for now, change interface later if needed
-        self._command(a.as_ref()).map(|_| ())
-    }
-
-    /// Run an mpv command. The arguments are passed as a reference to a slice.
-    ///
-    /// ```
-    /// let mut mpv = Mpv::connect("/tmp/mpvsocket")?;
-    /// mpv.command_arg("seek", &["0", "absolute"])?;
-    /// ```
-    pub fn command_arg<T: Into<Value> + Copy>(&mut self, command: &str, args: &[T]) -> Result<(), Error> {
-        let mut a = Vec::with_capacity(args.len() + 1);
-        a.push(command.into());
-        a.extend(args.iter().map(|v| (*v).into()));
-        self._command(a.as_ref()).map(|_| ())
+        self._command(once(command.into()).chain(args.into_iter().map(|v| v.into()))).map(|_| ())
     }
 
     /// Run an mpv command without any arguments.
@@ -177,7 +168,7 @@ impl Mpv {
     /// mpv.command("playlist-shuffle")?;
     /// ```
     pub fn command(&mut self, command: &str) -> Result<(), Error> {
-        self._command(&[command.into()]).map(|_| ())
+        self._command([command.into()].into_iter()).map(|_| ())
     }
 
     /// Retrieve a property from mpv.
@@ -187,7 +178,7 @@ impl Mpv {
     /// let paused = mpv.get_property("pause")?.as_bool().ok_or(Error::UnexpectedValue)?;
     /// ```
     pub fn get_property(&mut self, property: &str) -> Result<Value, Error> {
-        self._command(&["get_property".into(), property.into()])
+        self._command(["get_property".into(), property.into()].into_iter())
     }
 
     /// Set an mpv property to the given value.
@@ -197,7 +188,7 @@ impl Mpv {
     /// mpv.set_property("pause", true)?;
     /// ```
     pub fn set_property<T: Into<Value>>(&mut self, property: &str, value: T) -> Result<(), Error> {
-        self._command(&["set_property".into(), property.into(), value.into()]).map(|_| ())
+        self._command(["set_property".into(), property.into(), value.into()].into_iter()).map(|_| ())
     }
 
     /// Add the given value to an mpv property. Runs the 'add' mpv command.
@@ -207,8 +198,8 @@ impl Mpv {
     /// mpv.add_property("volume", 20.0)?;
     /// ```
     pub fn add_property(&mut self, property: &str, value: f64) -> Result<(), Error> {
-        self._command(&["add".into(), property.into(),
-            Number::from_f64(value).ok_or(Error::UnexpectedValue)?.into()]).map(|_| ())
+        self._command(["add".into(), property.into(),
+            Number::from_f64(value).ok_or(Error::UnexpectedValue)?.into()].into_iter()).map(|_| ())
     }
 
     /// Multiply an mpv property by the given value. Runs the 'multiply' mpv command.
@@ -218,18 +209,18 @@ impl Mpv {
     /// mpv.multiply_property("speed", 2.0)?;
     /// ```
     pub fn multiply_property(&mut self, property: &str, value: f64) -> Result<(), Error> {
-        self._command(&["multiply".into(), property.into(),
-            Number::from_f64(value).ok_or(Error::UnexpectedValue)?.into()]).map(|_| ())
+        self._command(["multiply".into(), property.into(),
+            Number::from_f64(value).ok_or(Error::UnexpectedValue)?.into()].into_iter()).map(|_| ())
     }
 
     /// Watch a property for changes. Runs the 'observe_property' mpv command.
     pub fn observe_property(&mut self, id: isize, property: &str) -> Result<(), Error> {
-        self._command(&["observe_property".into(), id.into(), property.into()]).map(|_| ())
+        self._command(["observe_property".into(), id.into(), property.into()].into_iter()).map(|_| ())
     }
 
     /// Undo the corresponding 'observe_property'. Runs the 'unobserve_property' mpv command.
     pub fn unobserve_property(&mut self, id: isize) -> Result<(), Error> {
-        self._command(&["unobserve_property".into(), id.into()]).map(|_| ())
+        self._command(["unobserve_property".into(), id.into()].into_iter()).map(|_| ())
     }
 
     /// Block until an mpv event occurs and return the event.
